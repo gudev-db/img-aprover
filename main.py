d99d86cb05d3608e330fa7eabafca90a213ffea8
Gustavo Henrique Romão <33608430+gudev-db@users.noreply.github.com>
2025-03-26 13:04:19 +0000

    m

--- main.py
+++ main.py
@@ -11,103 +11,55 @@ guias = """
 
 Comment: Usaria ícone ao lado das culturas para chamar atenção
 Created by: Holambra Cooperativa Agroindustrial
-Created at: 2025-02-06T19:25:15.769Z
 ----
 Comment: achei que ficou com muita informação
 Created by: Holambra Cooperativa Agroindustrial
-Created at: 2025-02-06T19:24:09.943Z
 ----
 Comment: Sugiro deixar a fonte mais de rodapé e trabalhar mais o título para chamar mais atenção
 Created by: Holambra Cooperativa Agroindustrial
-Created at: 2025-02-06T19:23:30.036Z
 ----
 Comment: Esse cenário ainda continua com a virada do ano? Vale revisar/atualizar?
 Created by: Holambra Cooperativa Agroindustrial
-Created at: 2025-02-06T18:57:47.386Z
 ----
 Comment: Será que vale falar para contarem com a Holambra na venda de sementes, tratamento e monitoramento?
 Created by: Holambra Cooperativa Agroindustrial
-Created at: 2025-02-06T18:51:31.840Z
 ----
 Comment: o sol ficou muito estourado, né?
 Created by: Holambra Cooperativa Agroindustrial
-Created at: 2025-02-06T18:50:12.319Z
 ----
 Comment: Como é sinal de atenção, colocaria um ícone para ilustrar
 Created by: Holambra Cooperativa Agroindustrial
-Created at: 2025-02-06T18:49:06.850Z
 ----
 Comment: Verificar se o vermelho está correto. Na minha tela pareceu rosa. Além disso, confirmar se é  essa foto que é antiga (sem as atualizações do parque industrial)
 Created by: Holambra Cooperativa Agroindustrial
-Created at: 2025-02-06T18:47:17.778Z
 ----
 Comment: @snjezana.abreu@holambra.com.br, validar com o Pitt. Pelo o que ouvi, ainda não é superior, mas até 400 mil. Talvez: armazenagem de até 400 mil toneladas
 Created by: Holambra Cooperativa Agroindustrial
-Created at: 2025-02-06T18:46:51.473Z
 ----
 Comment: As # precisam ter caixa alta e baixa? É melhor para identificação?
 
 EmCampo, DaSoja...
 Created by: Holambra Cooperativa Agroindustrial
-Created at: 2025-01-30T12:15:12.324Z
 ----
 Comment: não gostei do gerúndio, talvez tirar?
 
 ... dessa jornada com soluções cada vez mais eficientes e sustentáveis
 Created by: Holambra Cooperativa Agroindustrial
-Created at: 2025-01-30T12:13:36.641Z
 ----
 Comment: No primeiro evento do ano, realizado em Itaberá-SP, reunimos cooperados, produtores...
 Created by: Holambra Cooperativa Agroindustrial
-Created at: 2025-01-30T12:08:52.726Z
 ----
 Comment: Sugestão: 
 
 🌱Holambra em Campo 2025: conectando tecnologia e inovação na cultura da soja!
 
  
-No primeiro evento do ano, reunimos cooperados, produtores, especialistas e empresas parceiras para compartilhar conhecimento, apresentar tendências e debater as inovações que estão transformando o campo.
 
-Agradecemos a todos que participaram e às empresas que fazem parte dessa jornada, fortalecendo com soluções cada vez mais eficientes e sustentáveis.
-
-🎥 Confira no vídeo os melhores momentos do Holambra em Campo e veja como, juntos, estamos cultivando o futuro da produção agrícola!
-Created by: Snjezana Simunovic
-Created at: 2025-01-28T16:45:37.150Z
-----
-Comment: cooperados, produtores, especialistas e empresas parceiras
-Created by: Snjezana Simunovic
-Created at: 2025-01-28T16:39:27.662Z
-----
-Comment: Algo mais nese sentido:
-
-Demos início à programação de eventos de 2025 com o primeiro Holambra em Campo do ano, conectando tecnologia e inovação à cultura da soja!
-Created by: Snjezana Simunovic
-Created at: 2025-01-28T16:38:12.104Z
-----
-Comment: retirar
-Created by: Snjezana Simunovic
-Created at: 2025-01-28T16:32:55.861Z
-----
-Comment: Sugestão:
-
-Parabéns, Santa Cruz do Rio Pardo! 
-
-São 155 anos de história, crescimento e desenvolvimento! Desde julho de 2024, temos a honra de fazer parte com nossa loja, contribuindo para o progresso dos produtores locais e fortalecendo o agronegócio da região.
-
-Queremos seguir ao lado dos nossos clientes santa-cruzenses, apoiando cada nova conquista! 
-
-#HolambraCooperativa #Cooperativa #SantaCruzdoRioPardo
-Created by: Snjezana Simunovic
-Created at: 2025-01-20T17:50:04.526Z
-----
 Comment: Gosto mais dessa opção, mas não vejo qual a relevância dessa infermoção para o nosso negócio, não temos produtores de arroz.
 
 Conforme combinado, nos aniversários das cidades, utilizaremos uma foto da cidade junto com a da loja ou unidade correspondente.
 Created by: Snjezana Simunovic
-Created at: 2025-01-20T14:34:08.755Z
-----
-comments_2025-02-27T11:12:29.725Z.txt
-Displaying comments_2025-02-27T11:12:29.725Z.txt.
+
 """
 
 
