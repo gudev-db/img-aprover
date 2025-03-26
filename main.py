9893f10bd341cc6682e5a2f3c5bea9a67f81b186
Gustavo Henrique Romão <33608430+gudev-db@users.noreply.github.com>
2025-03-26 13:02:35 +0000

    m

--- main.py
+++ main.py
@@ -182,15 +182,6 @@ if tipo_aprovacao == "Imagem":
 
         BEGIN DIRETRIZES
         aprenda com as diretrizes de aprovação: {conteudo}
-        aprenda com as diretrizes de aprovação: {conteudo}
-        aprenda com as diretrizes de aprovação: {conteudo}
-        aprenda com as diretrizes de aprovação: {conteudo}
-        aprenda com as diretrizes de aprovação: {conteudo}
-        aprenda com as diretrizes de aprovação: {conteudo}
-        aprenda com as diretrizes de aprovação: {conteudo}
-
-            
-
 
         END DIRETRIZES
          
